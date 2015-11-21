0cc4f95ee2df8e3494183be4466529333cd9b6ea
UdaySrilu <suthapalli.uday@gmail.com>
2015-11-21 19:32:38 +0530

    firstCopy2

---
+++ app/src/main/java/com/example/sekhar/webaccess/DataToPost.java
@@ -0,0 +1,34 @@
+package com.example.sekhar.webaccess;
+
+/**
+ * Created by Sekhar on 07/11/2015.  test
+ */
+public class DataToPost {
+    private String name;
+    private String country;
+    private String twitter;
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+}
